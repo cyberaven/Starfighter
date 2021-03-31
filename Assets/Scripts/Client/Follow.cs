2e3578c5f7f377672761ca0214ad87b053691a9a
Lndael <Lndael10@gmail.com>
2021-03-31 22:52:26 +0300

    Navigator scene updated

--- Assets/Scripts/Client/Follow.cs
+++ Assets/Scripts/Client/Follow.cs
@@ -1,18 +1,28 @@
-﻿using UnityEngine;
+﻿using Client;
+using Client.Core;
+using UnityEngine;
 
 public class Follow : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Player;
     private Vector3 offset;
+    public Camera navigatorCamera;
     void Start()
-    {
+        {
         offset = Vector3.zero + Vector3.up*10;
-    }
+        }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = Player.transform.position + offset;
+        ZoomCamera();
+    }
+    void ZoomCamera()
+    {
+        navigatorCamera.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * 50;
+        if(navigatorCamera.orthographicSize < 20) navigatorCamera.orthographicSize = 20;
+        if(navigatorCamera.orthographicSize > 200) navigatorCamera.orthographicSize = 200;
     }
 }
