af157b9ecd41ce42f6bdb0abea64cc2872775993
Lndael <Lndael10@gmail.com>
2021-04-04 19:48:59 +0300

    камера шевелится, зум подправил ну и там всякие спрайты/шейдеры

---
+++ Assets/CameraMotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    private Camera _camera;
+
+    private Vector3 _translationPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _translationPoint = new Vector3(Input.GetAxis("Horizontal") * Camera.main.orthographicSize / 20,Input.GetAxis("Vertical") * Camera.main.orthographicSize / 20,0);
+        _camera.transform.Translate(_translationPoint*-1);
+        
+    }
+}
