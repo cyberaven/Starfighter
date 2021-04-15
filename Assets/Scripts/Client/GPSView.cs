bd936fc9db5e0a6073aaed42fb2dd0756f0eabc3
Lndael <42750761+Lndael@users.noreply.github.com>
2021-04-15 21:21:01 +0300

    no message

---
+++ Assets/Scripts/Client/GPSView.cs
@@ -0,0 +1,31 @@
+﻿using Client.Movement;
+using UnityEngine;
+
+namespace Client
+{
+    public class GPSView : MonoBehaviour
+    { public GameObject ship; 
+        private GameObject _target;
+        private Vector3 _lastPosition;
+
+        public void Init(PlayerScript playerScript, GameObject target = null)
+        {
+            Debug.unityLogger.Log($"CourseView: {gameObject.name} , {playerScript.gameObject.name}");
+            ship = playerScript.gameObject;
+            if (target is null) 
+                _target = ship;
+        }
+
+        public void SetTarget(GameObject target)
+        {
+            _target = target;
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = ship.transform.position;
+            transform.LookAt(_target.transform);
+        }
+    }
+}
