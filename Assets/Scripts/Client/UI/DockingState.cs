8a83683f537f3f44814051c9d71dd3bf13227589
Lndael <42750761+Lndael@users.noreply.github.com>
2021-07-30 00:05:50 +0300

    no message

---
+++ Assets/Scripts/Client/UI/DockingState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockingState : MonoBehaviour
+{
+    private Boolean _state;
+
+    [SerializeField] private Camera _cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _state = false;
+    }
+    
+    public void SwitchState()
+    {
+        if (_state == false)
+        {
+            _state = true;
+            _cam.cullingMask |= (1 << 10);
+        }
+        else if (_state == true)
+        {
+            _state = false;
+            _cam.cullingMask &= ~(1 << 10);
+        }
+    }
+}
---
+++ Assets/Scripts/Client/UI/DockingTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockingTrigger : MonoBehaviour
+{
+    [SerializeField] private Boolean _isDocked = false;
+    void OnTriggerStay(Collider myTrigger) 
+    {
+        _isDocked = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isDocked = false;
+    }
+}
