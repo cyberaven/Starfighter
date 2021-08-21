f5e5976f663d0489dbb4ec6b8058e7a3cb60caca
Lndael <42750761+Lndael@users.noreply.github.com>
2021-08-21 11:22:49 +0300

    no message

--- Assets/Scripts/Client/UI/DockingTrigger.cs
+++ Assets/Scripts/Client/UI/DockingTrigger.cs
@@ -1,11 +1,17 @@
-﻿using Net.Core;
+﻿using System;
+using Net.Core;
 using UnityEngine;
 
 namespace Client.UI
 {
     public class DockingTrigger : BasePlayerUIElement
     {
-        
+        private GameObject _button;
+        public void Start()
+        {
+            _button = GameObject.Find("LockButton");
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             var otherUnit = other.gameObject.transform.parent.gameObject;
@@ -13,12 +19,14 @@ namespace Client.UI
             {
                 PlayerScript.lastThingToDock = otherUnit;
                 PlayerScript.readyToDock = true;
+                _button.SetActive(true);
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
             PlayerScript.readyToDock = false;
+            _button.SetActive(false);
         }
         
         
